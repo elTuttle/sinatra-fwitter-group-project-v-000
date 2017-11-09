374d34451a0458e2d81f4f8152be2abd702793a5
John Tuttle <thejwt@gmail.com>
2017-11-09 21:48:45 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -97,7 +97,8 @@ class ApplicationController < Sinatra::Base
     if params[:username] == ""||params[:password] == ""
       redirect to '/login'
     else
-      user = User.find_by(username: params[:username], password_digest: params[:password])
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect to '/tweets'
     end
